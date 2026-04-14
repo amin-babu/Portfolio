8ec5ba412a07761397044f96f3d9c030e9dc442f
aminbabu12 <badrulaminbabu@gmail.com>
2026-04-14 18:46:00 +0600

    project updated

---
+++ script.js
@@ -0,0 +1,34 @@
+gsap.from('.header_right', {
+  x: 200,
+  duration: 1,
+  opacity: 0,
+  delay: 0.2
+});
+
+gsap.from('.headlin', {
+  opacity: 0,
+  x: -50,
+  duration: 0.7,
+  delay: 1.2
+});
+
+gsap.from('.header_left h1', {
+  opacity: 0,
+  x: -50,
+  duration: 0.7,
+  delay: 1.9
+});
+
+gsap.from('.headlin2', {
+  opacity: 0,
+  x: -50,
+  duration: 1,
+  delay: 2.6
+});
+
+gsap.from('.header_left a', {
+  opacity: 0,
+  duration: 1,
+  delay: 3.6
+});
+
